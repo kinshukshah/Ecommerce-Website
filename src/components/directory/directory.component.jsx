cc3ff44eb7dfb09773eddd1aee0b7391418b8a48
Kinshuk Shah <kinshukshah@gmail.com>
2020-07-10 17:29:40 +0530

    update our menu-items to navigate with router HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,26 +11,26 @@ class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: ''
                 },
                 {
                   title: 'sneakers',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: ''
                 },
                 {
                   title: 'womens',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens',
+                  linkUrl: '',
                   sizes:'large',
                 },
                 {
@@ -38,7 +38,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens',
+                  linkUrl: '',
                   sizes:'large',
                 }
               ],
@@ -51,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({title,imageUrl,id,sizes}) =>(
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={sizes}></MenuItem>
+                this.state.sections.map(({id,...otherSectionProps}) =>(
+                    <MenuItem key={id} {...otherSectionProps}></MenuItem>
                 ))
             }
             </div>
